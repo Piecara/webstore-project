50511f6d99d20f0d1c937059d6785eb76aebd6b0
Piecara <dominikwawrzkiewicz141@gmail.com>
2019-03-21 19:07:30 +0100

    initial commit

---
+++ webstore/src/main/java/com/dwsj/webstore/WebstoreApplication.java
@@ -0,0 +1,13 @@
+package com.dwsj.webstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebstoreApplication.class, args);
+	}
+
+}
